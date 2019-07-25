bd95aafa10c5416e3319f6dfb64f590167e16373
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-07-26 00:07:29 +0500

    test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import Header from '../Header';
 import RandomPlanet from '../Random-planet';
 import PeoplePage from '../People-page';
+import ItemList from '../Item-list';
+import PersonDetails from '../Person-details';
 
 import './App.css';
 
@@ -16,8 +18,14 @@ export default class App extends Component {
 
                 <RandomPlanet />
                 <PeoplePage />
-                <PeoplePage />
-                <PeoplePage />
+                {/* <div className='row mb-3'>
+                    <div className='col-6'>
+                        <ItemList onPersonSelected={this.onPersonSelected} />
+                    </div>
+                    <div className='col-6'>
+                        <PersonDetails personId={this.state.selectedPerson} />
+                    </div>
+                </div>    */}
             </div>
         );
     }
