d2d006d07f7f9c7f1066d92326f3b62370b4716a
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-07-31 01:05:53 +0500

    Adding propTypes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -37,7 +37,7 @@ export default class App extends Component {
                 <div className="stardb-app">
                     <Header onServiceChange={this.onServiceChange}/>
 
-                    <RandomPlanet/>
+                    <RandomPlanet />
 
                     <PeoplePage />
                     <PlanetPage />
