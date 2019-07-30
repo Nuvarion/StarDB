74cd57aef2bb86ffd15512369a22018fe493906f
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-07-30 22:41:32 +0500

    Adding compose

--- src/components/sw-component/Item-lists.js
+++ src/components/sw-component/Item-lists.js
@@ -1,57 +1,51 @@
 import React from 'react';
 
 import ItemList from '../Item-list';
-import { withData, withSwapiService } from '../Hoc-helpers';
-
-const withChildFunction = (Wrapped, fn) => {
-    return (props) => {
-        return (
-            <Wrapped {...props}>
-                {fn}
-            </Wrapped>
-        )
-    }
-};
+import { 
+    withData, 
+    withSwapiService,
+    withChildFunction,
+    compose } from '../Hoc-helpers';
 
 const renderName = ({ name }) => <span>{name}</span>;
 
-const renderModelAndName = ({ model, name }) => <span>{name} ({model})</span>
+const renderModelAndName = ({ model, name }) => <span>{name} ({model})</span>;
 
 const mapPersonMethodsToProps = (swapiService) => {
     return {
         getData: swapiService.getAllPeople
     }
-}
+};
 
 const mapPlanetMethodsToProps = (swapiService) => {
     return {
         getData: swapiService.getAllPlanets
     }
-}
+};
 
 const mapStarshipMethodsToProps = (swapiService) => {
     return {
         getData: swapiService.getAllStarships
     }
-}
+};
 
-const PersonList = withSwapiService(
-    withData(
-    withChildFunction(ItemList,renderName)),
-    mapPersonMethodsToProps
-    );
+const PersonList = compose(
+    withSwapiService(mapPersonMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+    )(ItemList);
 
-const PlanetList = withSwapiService(
-    withData(
-    withChildFunction(ItemList,renderName)),
-    mapPlanetMethodsToProps
-    );
+const PlanetList = compose(
+    withSwapiService(mapPersonMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+    )(ItemList);
 
-const StarshipList = withSwapiService(
-    withData(
-    withChildFunction(ItemList,renderModelAndName)),
-    mapStarshipMethodsToProps
-    );
+const StarshipList = compose (
+    withSwapiService(mapPersonMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+    )(ItemList);
 
 export {
     PersonList,
